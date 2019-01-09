bace94446be9820638d82a6210a4db588c5b343a
Kartik Verma <kvs1297@gmail.com>
2019-01-09 18:34:09 +0000

    some changes made for deployment

--- slaz/settings.py
+++ slaz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u^hclub)xbtxalb2)r7rk-(#3y9xqhkbney$2(hg00en!68-0*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['convosyn.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
