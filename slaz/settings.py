a12cb085131f857dc6a52e4a6ac796d7e8a15703
Kartik Verma <kvs1297@gmail.com>
2019-01-10 13:31:53 +0530

    made some changes

--- slaz/settings.py
+++ slaz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u^hclub)xbtxalb2)r7rk-(#3y9xqhkbney$2(hg00en!68-0*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['convosyn.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['convosyn.pythonanywhere.com']
 
 
 # Application definition
