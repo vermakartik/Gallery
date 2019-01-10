0dbded2f6ffac98465977846d27505e49918073e
Kartik Verma <kvs1297@gmail.com>
2019-01-11 00:20:36 +0530

    changed file field to image field

--- image_gallery/models.py
+++ image_gallery/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 import datetime
+from . import utils
+
 # Create your models here.
+def calculate_reduction_factor(width):
+    return (width / 512)
+
 class Gallery(models.Model):
     title_text = models.CharField(max_length = 100)
-    image_file = models.FileField(upload_to = 'documents/%Y/%m/%d/', blank=False, null=False)
+    image_file = models.ImageField(upload_to = 'documents/%Y/%m/%d/', blank=False, null=False)
     upload_time = models.DateField(auto_now = True)
     
     class Meta:
